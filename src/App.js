b3885cecb1e50e59c880058a533eefdd14b68fa7
Hiroyughi <35885324+Hiroyughi@users.noreply.github.com>
2019-07-10 15:50:26 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React from "react";
+import Weather from "./Weather";
+import Form from "./Form";
+import Titles from "./Titles";
+import './App.css';
+const Api_Key = "90ecdb3a0f90645083e604133dec5ce3";
+
+class App extends React.Component {
+
+  state = {
+
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  getWeather = async (e) => {
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    e.preventDefault();   
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
+    const response = await api_call.json();
+    console.log(response);
+    if(city && country){
+      this.setState({
+        temperature: response.main.temp,
+        city: response.name,
+        country: response.sys.country,
+        humidity: response.main.humidity,
+        description: response.weather[0].description,
+        error: ""
+      })
+    }else{
+      this.setState({
+        error: "Please input search values..."
+      })
+    }
+  }
+
+  render() {
+
+    return (
+
+      <div>
+         <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="title-container">
+                <Titles />
+                </div>
+                <div className="form-container">
+                <Form loadWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+}
+export default App;
